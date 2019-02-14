b9da4ff5c394573686c8b71ed1d61d99bfca835f
Tina Taleb <tinataleb@berkeley.edu>
2019-02-13 18:15:46 -0800

    Ready for Heroku!

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,15 +1,83 @@
 class HangpersonGame
 
+  # cd ..\..\..\..\Desktop\Spring19\CS169\Homework\hw-sinatra-saas-hangperson
+  # bundle exec autotest
+
+  attr_reader :word, :guesses, :wrong_guesses
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
   # Get a word from remote "random word" service
-
-  # def initialize()
-  # end
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+
+  def guess(letter)
+    # letter = letter.to_s.downcase
+    # @word.any?{|l| l.casecmp(letter)==0}
+    # if @word.include? letter and not @guesses.include? letter
+
+
+    if letter == nil or not (letter.class == String && letter =~ /^[A-z]$/i)
+      raise ArgumentError
+    end
+
+    letter.downcase!
+
+    if @guesses.include? letter or @wrong_guesses.include? letter or letter.length != 1
+      return false
+    end
+
+    if @word.include? letter
+      @guesses << letter
+    else
+      @wrong_guesses << letter
+    end
+    return true
+
+=begin
+    if @word.to_s.include? letter.to_s.downcase and not @guesses.to_s.include? letter.to_s.downcase
+      @guesses += letter
+      # puts guesses
+    # elsif not @word.include? letter
+    elsif not @word.to_s.downcase.include? letter.to_s.downcase
+      if not @wrong_guesses.to_s.downcase.include? letter.to_s.downcase
+      # if not @wrong_guesses.include? letter
+        @wrong_guesses += letter
+        # puts wrong_guesses
+      end
+    end
+=end
+  end
+
+
+
+  def word_with_guesses
+    result = ''
+    @word.split('').each do |char|
+      if @guesses.include? char
+        result << char
+      else
+        result << '-'
+      end
+    end
+
+    return result
+  end
+
+
+  def check_win_or_lose
+    if word_with_guesses == @word
+      return :win
+    elsif @wrong_guesses.length >= 7
+      return :lose
+    else
+      return :play
+    end
+
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
