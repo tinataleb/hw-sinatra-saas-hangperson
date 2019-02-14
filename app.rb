fe9f4c8c9b94d342be1c25cb05ddb81d52bd8cd1
Tina Taleb <tinataleb@berkeley.edu>
2019-02-13 19:01:40 -0800

    done with p3?

--- app.rb
+++ app.rb
@@ -39,8 +39,23 @@ class HangpersonApp < Sinatra::Base
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
-    ### YOUR CODE HERE ###
-    redirect '/show'
+
+    begin
+      if !@game.guess(letter)
+        flash[:message] = "You have already used that letter"
+      end
+      if @game.check_win_or_lose == :win
+        redirect '/win'
+      elsif @game.check_win_or_lose == :lose
+        redirect '/lose'
+      else
+        redirect '/show'
+      end
+    rescue ArgumentError
+      flash[:message] = "Invalid input"
+      redirect '/show'
+    end
+
   end
   
   # Everytime a guess is made, we should eventually end up at this route.
